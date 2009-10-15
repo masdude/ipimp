d6e9e1bf8d1f227af33e3ae1d74adb12cff84aed
martinvanderboon <martinvanderboon@6292b0b4-90ad-11de-9fd4-21bbefa17291>
2009-10-15 09:08:53 +0000

    Added TranscodeStatus page

--- MediaPortal Universal Web Interface/trunk/Website/My Project/AssemblyInfo.vb
+++ MediaPortal Universal Web Interface/trunk/Website/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.0.0.63")> 
-<Assembly: AssemblyFileVersion("4.0.0.63")> 
+<Assembly: AssemblyVersion("4.0.0.66")> 
+<Assembly: AssemblyFileVersion("4.0.0.66")> 
