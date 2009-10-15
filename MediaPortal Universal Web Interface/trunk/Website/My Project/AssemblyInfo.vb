c59b8e84509eded0b0fc068a32d59b52494bdc9a
martinvanderboon <martinvanderboon@6292b0b4-90ad-11de-9fd4-21bbefa17291>
2009-10-15 12:17:50 +0000

    Updated AboutiPiMP

--- MediaPortal Universal Web Interface/trunk/Website/My Project/AssemblyInfo.vb
+++ MediaPortal Universal Web Interface/trunk/Website/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.0.0.72")> 
-<Assembly: AssemblyFileVersion("4.0.0.72")> 
+<Assembly: AssemblyVersion("4.0.0.74")> 
+<Assembly: AssemblyFileVersion("4.0.0.74")> 
