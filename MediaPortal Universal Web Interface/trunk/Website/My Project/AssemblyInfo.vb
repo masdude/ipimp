8d11f080c5a406f728b47ebca0433f9989141370
martinvanderboon <martinvanderboon@6292b0b4-90ad-11de-9fd4-21bbefa17291>
2009-10-24 10:54:57 +0000

    Assembly version updates

--- MediaPortal Universal Web Interface/trunk/Website/My Project/AssemblyInfo.vb
+++ MediaPortal Universal Web Interface/trunk/Website/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.0.0.106")> 
-<Assembly: AssemblyFileVersion("4.0.0.106")> 
+<Assembly: AssemblyVersion("4.0.0.107")> 
+<Assembly: AssemblyFileVersion("4.0.0.107")> 
