6da0d01b45675a9b55a671f0122f1363b7f0403a
martinvanderboon <martinvanderboon@6292b0b4-90ad-11de-9fd4-21bbefa17291>
2009-10-24 06:55:58 +0000

    Added Faroese, Italian, Norwegian & Swedish language files (still in English at the moment)

--- MediaPortal Universal Web Interface/trunk/Website/My Project/AssemblyInfo.vb
+++ MediaPortal Universal Web Interface/trunk/Website/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.0.0.101")> 
-<Assembly: AssemblyFileVersion("4.0.0.101")> 
+<Assembly: AssemblyVersion("4.0.0.104")> 
+<Assembly: AssemblyFileVersion("4.0.0.104")> 
