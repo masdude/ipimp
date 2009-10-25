a37389b852de388983f70619624e878932e615db
martinvanderboon <martinvanderboon@6292b0b4-90ad-11de-9fd4-21bbefa17291>
2009-10-25 15:39:18 +0000

    Assembly version updates

--- MediaPortal Universal Web Interface/trunk/iPiMPTranscodeClient/My Project/AssemblyInfo.vb
+++ MediaPortal Universal Web Interface/trunk/iPiMPTranscodeClient/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.0.0.108")> 
-<Assembly: AssemblyFileVersion("4.0.0.108")> 
+<Assembly: AssemblyVersion("4.0.0.114")> 
+<Assembly: AssemblyFileVersion("4.0.0.114")> 
