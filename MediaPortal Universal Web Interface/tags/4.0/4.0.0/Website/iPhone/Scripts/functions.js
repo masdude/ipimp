0d08b8a9d142a5d8a5fc2b61b84be78dc104e187
martinvanderboon <martinvanderboon@6292b0b4-90ad-11de-9fd4-21bbefa17291>
2009-10-25 15:41:45 +0000

    Tagged release 4.0.0

---
+++ MediaPortal Universal Web Interface/tags/4.0/4.0.0/Website/iPhone/Scripts/functions.js
@@ -0,0 +1,163 @@
+﻿//version 4.0.0
+function logout() {
+    document.location = '#_Logout';
+    var btn = document.getElementById('aspBtnLogout');
+    if (btn == null)
+        alert('Button not found!');
+    else
+        btn.click();
+} 
+
+function createuser() {
+    var username = document.getElementById('jsUsername').value;
+    var password1 = document.getElementById('jsPassword1').value;
+    var password2 = document.getElementById('jsPassword2').value;
+    var recorder = document.getElementById('jsRecorder').checked;
+    var watcher = document.getElementById('jsWatcher').checked;
+    var deleter = document.getElementById('jsDeleter').checked;
+    var remoter = document.getElementById('jsRemoter').checked;
+    var admin = document.getElementById('jsAdmin').checked;
+
+    WA.Request('Admin/UserManagementAddUserResult.aspx?user=' + username + '&newpass=' + password1 + '&confpass=' + password2 + '&recorder=' + recorder + '&watcher=' + watcher + '&deleter=' + deleter + '&remoter=' + remoter + '&admin=' + admin + '#_CreateUser', null, -1, true, null);
+    return false;
+}
+
+function deleteuser() {
+    var doc = document.getElementById('jsMPUser');
+    var o = doc.childNodes;
+    var username = o[0].firstChild.innerHTML;
+
+    WA.Request('Admin/UserManagementDeleteConfirm.aspx?username=' + username + '#_DeleteUserConfirm', null, -1, true, null);
+    return false;
+}
+
+function changepassword() {
+    var oldpass = document.getElementById('jsOldPassword').value;
+    var newpass = document.getElementById('jsNewPassword1').value;
+    var confpass = document.getElementById('jsNewPassword2').value;
+
+    WA.Request('Admin/UserManagementChangePasswordResult.aspx?oldpass=' + oldpass + '&newpass=' + newpass + '&confpass=' + confpass + '#_ChangePass', null, -1, true, null);
+    return false;
+}
+
+function addmpclient() {
+    var friendly = document.getElementById('jsFriendly').value;
+    var hostname = document.getElementById('jsHostname').value;
+    var port = document.getElementById('jsPort').value;
+    var macaddress = document.getElementById('jsMAC').value;
+
+    WA.Request('Admin/ClientManagementAddResult.aspx?friendly=' + friendly + '&hostname=' + hostname + '&port=' + port + '&macaddress=' + macaddress + '#_ClientAddResult', null, -1, true, null);
+    return false;
+}
+
+function delmpclient() {
+    var doc = document.getElementById('jsMPClient');
+    var o = doc.childNodes;
+    var friendly = o[0].firstChild.innerHTML;
+
+    WA.Request('Admin/ClientManagementDeleteConfirm.aspx?friendly=' + friendly + '#_ClientDeleteConfirm', null, -1, true, null);
+    return false;
+}
+
+function updmpclient() {
+    var doc = document.getElementById('jsMPClient');
+    var o = doc.childNodes;
+    var friendly = o[0].firstChild.innerHTML;
+
+    WA.Request('Admin/ClientManagementUpdate.aspx?friendly=' + friendly + '#_ClientUpdate', null, -1, true, null);
+    return false;
+}
+
+function updmpclient2() {
+    var friendly = document.getElementById('jsFriendly').value;
+    var hostname = document.getElementById('jsHostname').value;
+    var port = document.getElementById('jsPort').value;
+    var macaddress = document.getElementById('jsMAC').value;
+
+    WA.Request('Admin/ClientManagementUpdateResult.aspx?friendly=' + friendly + '&hostname=' + hostname + '&port=' + port + '&macaddress=' + macaddress + '#_ClientUpdateResult', null, -1, true, null);
+    return false;
+}
+
+function tvsearch(group) {
+    document.getElementById('jsLoading').style.visibility = 'visible';
+    
+    var search = document.getElementById('jsTVSearchText').value;
+    
+    var doc = document.getElementById('jsTVSearchGenre');
+    var o = doc.childNodes;
+    var genre = o[0].firstChild.innerHTML;
+
+    WA.Request('TVGuide/SearchTVGuideResults.aspx?group=' + group + '&search=' + search + '&genre=' + genre + '#_SearchResults' + group, null, -1, true, null);
+    return false;
+}
+
+function deletemulti() {
+    var cbResults = '';
+    var doc = document.getElementsByTagName('input');
+    for (var i = 0; i < doc.length; i++) {
+        if (doc[i].checked == true) {
+            cbResults += doc[i].value + '-';
+        }
+    }
+    WA.Request('Recording/RecordingsMultiDeleteConfirm.aspx?ids=' + cbResults + '#_RecDeleteConfirm', null, -1, true, null);
+    return false;
+}
+
+function playtracks(wadiv, friendly, filter) {
+
+    div = document.getElementById(wadiv);
+    inputs = div.getElementsByTagName('input');
+    tracks = '';
+
+    for (i = 0; i < inputs.length; i++) {
+        if (inputs[i].id.substring(0, 9) == 'jsShuffle') { shuffle = inputs[i].checked; }
+        if (inputs[i].id.substring(0, 9) == 'jsEnqueue') { enqueue = inputs[i].checked; }
+        if (inputs[i].id.substring(0, 10) == 'MusicTrack') {
+            if (inputs[i].checked == true) { tracks += inputs[i].value + '+'; }
+        }
+    }
+    
+    if (tracks == '') {
+        for (j = 0; j < inputs.length; j++) {
+            if (inputs[j].id.substring(0, 10) == 'MusicTrack') { tracks += inputs[j].value + '+'; }
+        }
+    }
+    
+    WA.Request('MPClient/MyMusicPlayTracks.aspx?friendly=' + friendly + '&filter=' + filter + '&shuffle=' + shuffle + '&enqueue=' + enqueue + '&tracks=' + tracks + '#_MPClientPlayTracks', null, -1, true, null);
+    return false;
+}
+
+function musicsearch(friendly) {
+    var artist = document.getElementById('jsMusicSearchArtist').value;
+    var album = document.getElementById('jsMusicSearchAlbum').value;
+    var track = document.getElementById('jsMusicSearchTrack').value;
+    var genre = document.getElementById('jsMusicSearchGenre').value;
+    WA.Request('MPClient/MyMusicListTracksForSearch.aspx?friendly=' + friendly + '&search=' + escape(artist + ',' + album + ',' + track + ',' + genre) + '&start=0#_MyMusicSearchResults', null, -1, true, null);
+    return false;
+}
+
+function saveplaylist(friendly) {
+    var filename = document.getElementById('jsSaveFilename').value;
+    WA.Request('MPClient/MyMusicSavePlaylistResult.aspx?friendly=' + friendly + '&filename=' + filename + '#_MyMusicSavePlaylistResult', null, -1, true, null);
+    return false;
+}
+
+function sendmessage(friendly) {
+    var message = document.getElementById('jsMessage').value;
+    WA.Request('MPClient/MPClientSendMessageResult.aspx?friendly=' + friendly + '&message=' + message + '#_MPClientSendMessageResult', null, -1, true, null);
+    return false;
+}
+
+function sendkeystring(friendly) {
+    keystring = document.getElementById('jsKeyString').value;
+    WA.Request('MPClient/MCERemoteControlButton.aspx?friendly=' + friendly + '&button=' + keystring + '#_MCEButton', null, -1, false, null);
+    return false;
+}
+
+function updatesettings() {
+    var doc = document.getElementById('jsPageSize');
+    var o = doc.childNodes;
+    var pagesize = o[0].firstChild.innerHTML;
+    WA.Request('Admin/ManageSettingsResult.aspx?pagesize=' + pagesize+ '#_SettingsResult', null, -1, true, null);
+    return false;
+}
