45048f0af8b2033e5223f89a79041bbc4ccb234e
martinvanderboon <martinvanderboon@6292b0b4-90ad-11de-9fd4-21bbefa17291>
2009-11-01 10:00:01 +0000

    Segmenter update

--- MediaPortal Universal Web Interface/trunk/WinSegmenter/FFMpegWrapper.vb
+++ MediaPortal Universal Web Interface/trunk/WinSegmenter/FFMpegWrapper.vb
@@ -1,23 +1,44 @@
 ﻿Imports System.IO
 Imports System.Diagnostics
 
-Public Class Streams
+Public Class FFMpegWrapper
 
     Const chunk2 As Integer = 1048576
     Const chunk As Integer = 1023
 
+    Public Shared Sub DoIt()
 
-    Public Shared Sub CopyStreamContents(ByVal inStream As Stream)
+        Dim ffmpeg As Process = New Process()
 
-        ' assert these are the right kind of streams
-        If inStream Is Nothing Then Throw New ArgumentNullException("input")
-        If Not inStream.CanRead Then Throw New ArgumentException("Input stream must support CanRead")
+        Dim duration As String = String.Empty
+        Dim result As String = String.Empty
+        Dim eReader As StreamReader = Nothing
+        Dim oReader As StreamReader = Nothing
+
+        ffmpeg.StartInfo.UseShellExecute = False
+        ffmpeg.StartInfo.ErrorDialog = False
+        ffmpeg.StartInfo.RedirectStandardError = True
+        ffmpeg.StartInfo.RedirectStandardOutput = True
+        ffmpeg.StartInfo.FileName = "D:\temp\ffmpeg\ffmpeg.exe"
+        ffmpeg.StartInfo.Arguments = "-i D:\Temp\ffmpeg\test.ts " & _
+                                     "-threads 4 -re -vcodec libx264 -s 480x272 -flags +loop -cmp +chroma " & _
+                                     "-deblockalpha 0 -deblockbeta 0 -crf 24 -bt 256k -refs 1 -coder 0 -me_method umh " & _
+                                     "-me_range 16 -subq 5 -partitions +parti4x4+parti8x8+partp8x8 -g 250 " & _
+                                     "-keyint_min 25 -level 30 -qmin 10 -qmax 51 -trellis 2 -sc_threshold 40 " & _
+                                     "-i_qfactor 0.71 -acodec libfaac -ab 96k -ar 48000 -ac 2 -async 2 - |"
+
+
+        ffmpeg.Start()
+
+        eReader = ffmpeg.StandardError
+        oReader = ffmpeg.StandardOutput
+
+        'ffmpeg.WaitForExit(1000)
+        'result = eReader.ReadToEnd()
+        'duration = result.Substring(result.IndexOf("Duration: ") + ("Duration: ").Length, ("00:00:00.00").Length)
 
         ' prepare the output filestreams
         Dim outFile As FileStream = Nothing
-
-        ' skip if the input stream is empty (if seeking is supported)
-        If inStream.CanSeek Then If inStream.Length = 0 Then Exit Sub
 
         ' allocate buffer (if all pre-conditions are met)
         Dim buffer(chunk) As Byte
@@ -25,13 +46,14 @@ Public Class Streams
         Dim chunkCount As Integer = 1
         ' iterate read/writes between streams
         Dim i As Integer = 0
+        Dim s As Stream
 
         Do
             If outFile Is Nothing Then
                 outFile = New FileStream(String.Format("D:\temp\ffmpeg\out{0}.ts", chunkCount.ToString), FileMode.Create, FileAccess.Write, FileShare.None)
             End If
             i += count
-            count = inStream.Read(buffer, 0, count)
+            count = s.r.Read(buffer, 0, count)
             If count = 0 Then Exit Do
             outFile.Write(buffer, 0, count)
 
@@ -67,38 +89,8 @@ Public Class Streams
             End If
         Loop
 
+
+
     End Sub
 
-
-
-    Private Shared Function GetDuration(ByVal path As String) As String
-        Dim ffmpeg As Process = New Process()
-
-        Dim duration As String = String.Empty
-        Dim result As String = String.Empty
-        Dim eReader As StreamReader = Nothing
-
-        ffmpeg.StartInfo.UseShellExecute = False
-        ffmpeg.StartInfo.ErrorDialog = False
-        ffmpeg.StartInfo.RedirectStandardError = True
-        ffmpeg.StartInfo.FileName = "D:\temp\ffmpeg\ffmpeg.exe"
-        ffmpeg.StartInfo.Arguments = String.Format("-i {0}", path)
-
-        ffmpeg.Start()
-
-        eReader = ffmpeg.StandardError
-
-        ffmpeg.WaitForExit(1000)
-
-        result = eReader.ReadToEnd()
-
-        duration = result.Substring(result.IndexOf("Duration: ") + ("Duration: ").Length, ("00:00:00.00").Length)
-
-        Return duration
-
-    End Function
-
-
 End Class
-
-
--- MediaPortal Universal Web Interface/trunk/WinSegmenter/Module1.vb
+++ MediaPortal Universal Web Interface/trunk/WinSegmenter/Module1.vb
@@ -6,6 +6,9 @@ Module Module1
     Sub Main()
 
         Dim inputStream As Stream = Console.OpenStandardInput()
+
+        'Dim br As New IO.BinaryReader(IO.File.OpenRead("D:\Temp\ffmpeg\test.ts"))
+
         Streams.CopyStreamContents(inputStream)
 
     End Sub
