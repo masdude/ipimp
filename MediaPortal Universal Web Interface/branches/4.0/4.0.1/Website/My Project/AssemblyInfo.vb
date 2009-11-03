d1e9257a443fcdf8a6ecfcb47dd80b5d5b19e1fb
martinvanderboon <martinvanderboon@6292b0b4-90ad-11de-9fd4-21bbefa17291>
2009-11-03 07:28:12 +0000

    Fix for defect 17

--- MediaPortal Universal Web Interface/branches/4.0/4.0.1/Website/My Project/AssemblyInfo.vb
+++ MediaPortal Universal Web Interface/branches/4.0/4.0.1/Website/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.0.1.242")> 
-<Assembly: AssemblyFileVersion("4.0.1.242")> 
+<Assembly: AssemblyVersion("4.0.1.243")> 
+<Assembly: AssemblyFileVersion("4.0.1.243")> 
