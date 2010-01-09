1441f30833c28aaf6b9f87012f82720a3c33e8a0
martinvanderboon <martinvanderboon@6292b0b4-90ad-11de-9fd4-21bbefa17291>
2010-01-09 14:52:20 +0000

    Updated adaptive styles on admin menus

--- MediaPortal Universal Web Interface/trunk/Website/iPhone/Scripts/Adaptive.js
+++ MediaPortal Universal Web Interface/trunk/Website/iPhone/Scripts/Adaptive.js
@@ -1,10 +1,16 @@
 ﻿WA.AddEventListener("beginslide", function(evt) {
     if (evt.context[1][0] == "waSettings" || evt.context[1][0] == "waSettingsResult") {
         document.getElementById('iHeader').style.background = "#FF8000";
-        document.getElementById('WebApp').style.background = "#F5D0A9";
+        document.getElementById('WebApp').style.background = "#F5D0A9 url(WebApp/Design/img/bg.png)";
     } else if (evt.context[1][0] == "waClientMenu" || evt.context[1][0] == "waClientAddMenu" || evt.context[1][0] == "waClientAddResult" || evt.context[1][0] == "waClientDeleteMenu" || evt.context[1][0] == "waClientDeleteConfirm" || evt.context[1][0] == "waClientDelResult" || evt.context[1][0] == "waClientUpdateMenu" || evt.context[1][0] == "waClientUpdate" || evt.context[1][0] == "waClientUpdateResult") {
-        document.getElementById('iHeader').style.background = "#9A2EFE";
-        document.getElementById('WebApp').style.background = "#D0A9F5";
+        document.getElementById('iHeader').style.background = "#8000FF";
+        document.getElementById('WebApp').style.background = "#D0A9F5 url(WebApp/Design/img/bg.png)";
+    } else if (evt.context[1][0] == "waChangePasswordMenu" || evt.context[1][0] == "waChangePass" || evt.context[1][0] == "waCreateUserMenu" || evt.context[1][0] == "waCreateUser" || evt.context[1][0] == "waDeleteUserMenu" || evt.context[1][0] == "waDeleteUserConfirm" || evt.context[1][0] == "waDeleteUserResult") {
+        document.getElementById('iHeader').style.background = "#FF0000";
+        document.getElementById('WebApp').style.background = "#F5A9A9 url(WebApp/Design/img/bg.png)";
+    } else if (evt.context[1][0] == "waAboutiPiMP") {
+        document.getElementById('iHeader').style.background = "#088A08";
+        document.getElementById('WebApp').style.background = "#81F781 url(WebApp/Design/img/bg.png)";
     } else {
         document.getElementById('iHeader').style.background = "";
         document.getElementById('WebApp').style.background = "";
