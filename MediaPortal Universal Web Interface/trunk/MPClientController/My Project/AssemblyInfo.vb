301fb9bf0d62df90d3a30e34496d2c612ccf3770
martinvanderboon <martinvanderboon@6292b0b4-90ad-11de-9fd4-21bbefa17291>
2010-01-16 14:56:56 +0000

    Assembly version updates

--- MediaPortal Universal Web Interface/trunk/MPClientController/My Project/AssemblyInfo.vb
+++ MediaPortal Universal Web Interface/trunk/MPClientController/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("4.1.0.368")> 
-<Assembly: AssemblyFileVersion("4.1.0.368")> 
+<Assembly: AssemblyVersion("4.1.0.409")> 
+<Assembly: AssemblyFileVersion("4.1.0.409")> 
