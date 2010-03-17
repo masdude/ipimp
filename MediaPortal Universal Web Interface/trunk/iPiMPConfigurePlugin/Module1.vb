d76746c55852a85b3ec5247c84ea41a632b9aa36
martinvanderboon <martinvanderboon@6292b0b4-90ad-11de-9fd4-21bbefa17291>
2010-03-17 08:23:35 +0000

    Tweaks to the configure plugin.

--- MediaPortal Universal Web Interface/trunk/iPiMPConfigurePlugin/Module1.vb
+++ MediaPortal Universal Web Interface/trunk/iPiMPConfigurePlugin/Module1.vb
@@ -4,13 +4,15 @@ Imports System.Configuration
 
 Module Module1
 
+    Dim appSettings As NameValueCollection = ConfigurationManager.AppSettings
+    Dim layer As New TvBusinessLayer
+    Dim setting As Setting
+    Dim args() As String = Command.Split("=")
+    'Dim args() As String = Split("add=iPiMPTranscodeToMP4_Test=true", "=")
+
     Sub Main()
 
-        Dim appSettings As NameValueCollection = ConfigurationManager.AppSettings
         appSettings.Set("GentleConfigFile", Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) & "\Team MediaPortal\MediaPortal TV Server\gentle.config")
-
-        Dim args() As String = Command.Split("=")
-        'Dim args() As String = Split("add=iPiMPTranscodeToMP4_Delete=true", "=")
 
         If UBound(args) <> 2 Then
             Console.Write("Invalid parameter passed.")
@@ -18,40 +20,59 @@ Module Module1
             Exit Sub
         End If
 
-        Dim layer As New TvBusinessLayer
-        Dim setting As Setting
         Select Case args(0).ToLower
             Case "add"
                 Try
                     setting = layer.GetSetting(args(1))
                     setting.Value = args(2).ToString
                     setting.Persist()
-                    Console.Write("Setting " & args(1) & " to " & args(2) & ". !!!SUCCESS!!!" & vbNewLine)
+                    Console.WriteLine(String.Format("Setting {0} to {1}. Result is !!SUCCESS!!", args(1), args(2)))
                 Catch ex As Exception
-                    Console.Write("Setting " & args(1) & " to " & args(2) & ". !!!FAILED!!!" & vbNewLine)
+                    Console.WriteLine(String.Format("Setting {0} to {1}. Result is !!FAILURE!!", args(1), args(2)))
                     System.Environment.ExitCode = 1
+                Finally
+                    Dispose()
                 End Try
             Case "del"
                 Try
                     setting = layer.GetSetting(args(1))
                     setting.Remove()
-                    Console.Write("Removing " & args(1) & " !!!SUCCESS!!!" & vbNewLine)
+                    Console.WriteLine(String.Format("Removing {0}. Result is !!SUCCESS!!", args(1), args(2)))
                 Catch ex As Exception
-                    Console.Write("Removing " & args(1) & " !!!FAILED!!!" & vbNewLine)
+                    Console.WriteLine(String.Format("Removing {0}. Result is !!FAILURE!!", args(1), args(2)))
                     System.Environment.ExitCode = 1
+                Finally
+                    Dispose()
                 End Try
             Case "get"
                 Try
                     setting = layer.GetSetting(args(1))
-                    Console.Write(setting.Value.ToString)
+                    Dim result As String = setting.Value.ToString
+                    If result = "" Then
+                        Console.WriteLine(String.Format("Setting {0} is not set.", args(1), args(2)))
+                    Else
+                        Console.WriteLine(setting.Value.ToString)
+                    End If
                 Catch ex As Exception
-                    Console.Write("!!!ERROR!!!" & vbNewLine)
+                    Console.WriteLine(String.Format("There was an error."))
                     System.Environment.ExitCode = 1
+                Finally
+                    Dispose()
                 End Try
             Case Else
-                Console.Write("Invalid action passed.")
+                Console.WriteLine("Invalid action passed.")
                 System.Environment.ExitCode = 1
+                Dispose()
         End Select
+
+
+    End Sub
+
+    Private Sub Dispose()
+        Setting = Nothing
+        layer = Nothing
+        args = Nothing
+        appSettings = Nothing
     End Sub
 
 End Module
