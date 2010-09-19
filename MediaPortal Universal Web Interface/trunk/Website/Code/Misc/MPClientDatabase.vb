7c1cf148357a6e9145cbb8444628daff9ee7f017
martinvanderboon <martinvanderboon@6292b0b4-90ad-11de-9fd4-21bbefa17291>
2010-09-19 12:58:15 +0000

    Added MPClients auto-register with iPiMP web app using UDP broadcasts.

--- MediaPortal Universal Web Interface/trunk/Website/Code/Misc/MPClientDatabase.vb
+++ MediaPortal Universal Web Interface/trunk/Website/Code/Misc/MPClientDatabase.vb
@@ -48,6 +48,31 @@ Namespace uWiMP.TVServer
 
         End Function
 
+        Public Shared Function IsExistingClientByHostname(ByVal hostname As String) As Boolean
+
+            Dim clientExists As Boolean = False
+
+            Dim connStr As String = ConfigurationManager.ConnectionStrings("uWiMPConnString").ConnectionString
+            Dim conn As SQLiteConnection = New SQLiteConnection(connStr)
+            Dim reader As SQLiteDataReader
+
+            Dim cmd As SQLiteCommand = New SQLiteCommand("SELECT Hostname FROM `MPClients` WHERE Hostname = $Hostname", conn)
+            cmd.Parameters.Add("$Hostname", DbType.String, 255).Value = hostname.ToLower
+
+            Try
+                conn.Open()
+                reader = cmd.ExecuteReader(CommandBehavior.Default)
+                If reader.HasRows Then clientExists = True
+            Catch ex As SQLiteException
+                Return False
+            Finally
+                conn.Close()
+            End Try
+
+            Return clientExists
+
+        End Function
+
         Public Shared Function GetClients() As List(Of uWiMP.TVServer.MPClient.Client)
 
             Dim mpclients As New List(Of uWiMP.TVServer.MPClient.Client)
@@ -113,6 +138,39 @@ Namespace uWiMP.TVServer
 
         End Function
 
+        Public Shared Function GetClientByHostname(ByVal hostname As String) As uWiMP.TVServer.MPClient.Client
+
+            Dim MPClient As New uWiMP.TVServer.MPClient.Client
+
+            Dim connStr As String = ConfigurationManager.ConnectionStrings("uWiMPConnString").ConnectionString
+            Dim conn As SQLiteConnection = New SQLiteConnection(connStr)
+            Dim reader As SQLiteDataReader
+
+            Dim cmd As SQLiteCommand = New SQLiteCommand("SELECT Friendly, MACAddress, Port FROM `MPClients` WHERE Hostname = $Hostname", conn)
+            cmd.Parameters.Add("$Hostname", DbType.String, 255).Value = hostname.ToLower
+
+            Try
+                conn.Open()
+                reader = cmd.ExecuteReader()
+                If reader.HasRows Then
+                    While reader.Read()
+                        MPClient.Friendly = reader.GetString(0)
+                        MPClient.Hostname = hostname
+                        MPClient.MACAddress = reader.GetString(1)
+                        MPClient.Port = reader.GetString(2)
+                    End While
+                End If
+                reader.Close()
+            Catch ex As SQLiteException
+                Return MPClient
+            Finally
+                conn.Close()
+            End Try
+
+            Return MPClient
+
+        End Function
+
         Public Shared Function ManageClient(ByVal client As uWiMP.TVServer.MPClient.Client, ByVal action As String) As Boolean
 
             If (action.ToLower = "insert") And (IsExistingClient(client.Friendly)) Then Return False
@@ -130,7 +188,11 @@ Namespace uWiMP.TVServer
                 Case "insert"
                     cmd.CommandText = "INSERT INTO `MPClients` (Friendly, Hostname, Port, MACAddress) VALUES ($Friendly, $Hostname, $Port, $MACAddress)"
                 Case "update"
-                    cmd.CommandText = "UPDATE `MPClients` SET Hostname = $Hostname, Port = $Port, MACAddress = $MACAddress WHERE Friendly = $Friendly"
+                    If IsExistingClient(client.Friendly) Then
+                        cmd.CommandText = "UPDATE `MPClients` SET Hostname = $Hostname, Port = $Port, MACAddress = $MACAddress WHERE Friendly = $Friendly"
+                    Else
+                        cmd.CommandText = "UPDATE `MPClients` SET Friendly = $Friendly, Port = $Port, MACAddress = $MACAddress WHERE Hostname = $Hostname"
+                    End If
             End Select
 
             If (action.ToLower = "insert") Or (action.ToLower = "update") Then
