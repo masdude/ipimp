8f0c2c8ddc9355b2b46175daabd2ebb5fbc72fea
martinvanderboon <martinvanderboon@6292b0b4-90ad-11de-9fd4-21bbefa17291>
2010-09-28 19:27:58 +0000

    ...and now it works!

--- MediaPortal Universal Web Interface/trunk/MPClientControllerConsoleTester/Module1.vb
+++ MediaPortal Universal Web Interface/trunk/MPClientControllerConsoleTester/Module1.vb
@@ -7,63 +7,17 @@ Module Module1
         Dim friendly As String = "diningroom"
         Dim request As New uWiMP.TVServer.MPClient.Request
         request.Action = "ping"
-        
+
         For Each arg As String In My.Application.CommandLineArgs
-
-            If arg.ToLower.StartsWith("/friendly=") Then
-                friendly = arg.Remove(0, 10)
-            Else
-                friendly = "diningroom"
-            End If
-
-            If arg.ToLower.StartsWith("/action=") Then
-                request.Action = arg.Remove(0, 8)
-            Else
-                request.Action = "ping"
-            End If
-
-            If arg.ToLower.StartsWith("/filter=") Then
-                request.Filter = arg.Remove(0, 8)
-            Else
-                request.Filter = ""
-            End If
-
-            If arg.ToLower.StartsWith("/value=") Then
-                request.Value = arg.Remove(0, 7)
-            Else
-                request.Value = ""
-            End If
-
-            If arg.ToLower.StartsWith("/start=") Then
-                request.Start = CInt(arg.Remove(0, 7))
-            Else
-                request.Start = 0
-            End If
-
-            If arg.ToLower.StartsWith("/pagesize=") Then
-                request.PageSize = CInt(arg.Remove(0, 10))
-            Else
-                request.PageSize = 0
-            End If
-
-            If arg.ToLower.StartsWith("/shuffle=") Then
-                request.Shuffle = CBool(arg.Remove(0, 9))
-            Else
-                request.Shuffle = False
-            End If
-
-            If arg.ToLower.StartsWith("/enqueue=") Then
-                request.Enqueue = CBool(arg.Remove(0, 9))
-            Else
-                request.Enqueue = False
-            End If
-
-            If arg.ToLower.StartsWith("/tracks=") Then
-                request.Tracks = arg.Remove(0, 8)
-            Else
-                request.Tracks = ""
-            End If
-
+            If arg.ToLower.StartsWith("/friendly=") Then friendly = arg.Remove(0, 10)
+            If arg.ToLower.StartsWith("/action=") Then request.Action = arg.Remove(0, 8)
+            If arg.ToLower.StartsWith("/filter=") Then request.Filter = arg.Remove(0, 8)
+            If arg.ToLower.StartsWith("/value=") Then request.Value = arg.Remove(0, 7)
+            If arg.ToLower.StartsWith("/start=") Then request.Start = CInt(arg.Remove(0, 7))
+            If arg.ToLower.StartsWith("/pagesize=") Then request.PageSize = CInt(arg.Remove(0, 10))
+            If arg.ToLower.StartsWith("/shuffle=") Then request.Shuffle = CBool(arg.Remove(0, 9))
+            If arg.ToLower.StartsWith("/enqueue=") Then request.Enqueue = CBool(arg.Remove(0, 9))
+            If arg.ToLower.StartsWith("/tracks=") Then request.Tracks = arg.Remove(0, 8)
         Next
 
         SendReceiveMessage(friendly, request)
